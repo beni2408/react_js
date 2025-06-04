b52457f56c5a78d4c46d724454fae56fb8133aa1
beni2408 <jascarbenish@gmail.com>
2025-06-04 20:31:17 +0530

    Started Learning  reactJS

---
+++ 279-intro-to-jsx/src/index.js
@@ -0,0 +1,10 @@
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+ReactDOM.createRoot().render(<div><h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"))
